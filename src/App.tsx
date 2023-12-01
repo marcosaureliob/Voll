20d582b3a3562d348b132e0be6f2d88d465c086f
Marcos Aurelio <77805407+marcosaureliob@users.noreply.github.com>
2023-12-01 17:09:43 -0300

    inclusao do grafico

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
 import './App.css';
 import Cabecalho from './components/Cabecalho';
 import Container from './components/Container';
+import Grafico from './components/Grafico';
 import Rodape from './components/Rodape';
 import Tabela from './components/Tabela';
 import Titulo from './components/Titulo';
 import useDadosConsulta from './useDadosConsulta';
+import useDadosProfissional from './useDadosProfissionais';
 
 function App() {
-  const { dados} = useDadosConsulta();
+  const { dados: consultas, erro: consultasErro } = useDadosConsulta();
+  const { dados: profissionais, erro: profissionaisErro } = useDadosProfissional();
+
+  if (consultasErro || profissionaisErro) {
+    console.log("ocorreu um erro na requisição")
+  }
+
   return (
     <>
       <Cabecalho />
       <Container>
         <Titulo>Área Administrativa</Titulo>
-        <Tabela consultas={dados} />
+        <Tabela consultas={consultas} />
+        <Grafico consultas={consultas} profissionais={profissionais}/>
       </Container>
       <Rodape />
     </>
